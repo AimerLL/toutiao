049254ecad850211f8e3951eba1e87e4d34aa171
Aimer <Amier@qq.com>
2020-02-29 14:37:46 +0800

    初次提交

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+// 在这里完成了 Vue的实例化 并且已经挂载了路由
