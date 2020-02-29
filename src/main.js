3d2882ba9286ee12b7163760ba9eb1140d4dad51
Aimer <Amier@qq.com>
2020-02-29 20:27:26 +0800

    初始化项目-整理目录-准备资源完毕

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入UI模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less' // 引入样式=>初始化工作
 Vue.config.productionTip = false
+Vue.use(ElementUI)// 全局注册element模块，所有的位置都可以使用组件 这样就能为所欲为了
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+// 引入主页
+import Home from '@/views/home'
+// 引入登录页
+import Login from '@/views/login'
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+  path: '/',
+  redirect: '/home'// 强制跳转到home页
+},
+{
+  path: '/home',
+  name: 'Home',
+  component: Home
+},
+{
+  path: '/login',
+  name: 'login',
+  component: Login
+}
+  // {
+  // path: '/about',
+  // name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
